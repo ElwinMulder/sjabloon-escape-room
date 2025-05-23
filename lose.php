2e2e7fc0530795dbd02e3f733b22d17a789ba50e
DylanoNv <9015493@student.zadkine.nl>
2025-05-23 11:44:09 +0200

    verlies en win scherm

---
+++ lose.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <title>Escape Room - Verloren</title>
+  <link rel="stylesheet" href="style.css">
+  <style>
+
+    body {
+      background-color: #000000;
+      background-image: url("afbeeldingen/verliesscherm.png");
+      background-size: cover;
+      background-position: center;
+      color: #ff3c3c;
+      font-family: 'Arial', sans-serif;
+      text-align: center;
+      margin: 0;
+      padding-top: 100px;
+    }
+
+    h1 {
+      font-size: 60px;
+      text-shadow: 0 0 10px #ff0000;
+    }
+
+    p {
+      font-size: 24px;
+      margin-top: 30px;
+    }
+  </style>
+</head>
+<body>
+  <h1>Je bent gepakt in Vault 666...</h1>
+  <p>Het kwaad heeft gewonnen...</p>
+</body>
+</html>
---
+++ win.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <title>Escape Room - Gehaald!</title>
+  <link rel="stylesheet" href="style.css">
+  <style>
+    body {
+      background-color: #0b0b0b;
+      background-image: url("afbeeldingen/winscherm.png");
+      background-size: cover;
+      background-position: center;
+      color: #00ff88;
+      font-family: 'Arial', sans-serif;
+      text-align: center;
+      margin: 0;
+      padding-top: 100px;
+    }
+
+    h1 {
+      font-size: 60px;
+      text-shadow: 0 0 10px #00ff88;
+    }
+
+    p {
+      font-size: 24px;
+      margin-top: 30px;
+    }
+  </style>
+</head>
+<body>
+  <h1>Je hebt Vault 666 overleefd...</h1>
+  <p>Het monster slaapt weer — voorlopig...</p>
+</body>
+</html>
